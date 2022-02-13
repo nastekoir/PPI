9f57f8a1b65e365021736bd7e7db4516f67c0cb3
Ilya <nastekoir@gmail.com>
2022-02-13 16:33:22 +0300

    Добавил методы интерфейса сервиса идентификации

--- Программа/PPI/ConsoleApp1/Bl/Services/IdentityService.cs
+++ Программа/PPI/ConsoleApp1/Bl/Services/IdentityService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ConsoleApp1.Bl.Interfaces;
+using ConsoleApp1.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,21 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1.Services
 {
-    public class IdentityService
+    public class IdentityService : IIdentityService
     {
-        
+        public bool AuthorizeUser(string userName, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CheckIsUserExist(Guid userID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ResgisterNewUser(IUser user)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
