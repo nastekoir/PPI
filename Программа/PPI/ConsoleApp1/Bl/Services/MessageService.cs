2843d58ceb31485a7b56dff4f5bc60c0e3dfef75
Ilya <nastekoir@gmail.com>
2022-02-13 16:15:13 +0300

    Добавил класс реализации сервиса сообщений

---
+++ Программа/PPI/ConsoleApp1/Bl/Services/MessageService.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleApp1.Bl.Interfaces;
+using ConsoleApp1.Models;
+
+namespace ConsoleApp1.Bl.Services;
+public class MessageService : IMessageService
+{
+    public void DeleteMessage(Guid messageId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SendMessage(Guid branchId, Message message)
+    {
+        throw new NotImplementedException();
+    }
+}
+
