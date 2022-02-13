fb1e5b9bc1246e503c0fc93a2c16839899edbd27
Ilya <nastekoir@gmail.com>
2022-02-13 16:55:29 +0300

    no message

--- Программа/PPI/ConsoleApp1/Bl/Services/MessageService.cs
+++ Программа/PPI/ConsoleApp1/Bl/Services/MessageService.cs
@@ -11,7 +11,7 @@ public class MessageService : IMessageService
 
     public void EditMessage(Guid messageId, string messageText)
     {
-        //UPDATE MessageTeble SET MessageText=messageText WHERE Id=messageIdфыв
+        //UPDATE MessageTeble SET MessageText=messageText WHERE Id=messageId321
     }
 
     public void SendMessage(Guid branchId, Message message)
