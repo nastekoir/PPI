a49082fd3d0a624c37f8ee1badb835b301f7b391
Ilya <nastekoir@gmail.com>
2022-02-13 16:25:42 +0300

    Добавил методы реализации интерфейса для работы с ветками обсуждений

--- Программа/PPI/ConsoleApp1/Bl/Services/BranchService.cs
+++ Программа/PPI/ConsoleApp1/Bl/Services/BranchService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleApp1.Bl.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,16 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1.Services
 {
-    internal class BranchService
+    public class BranchService : IBranchService
     {
+        public void CreateBranch(string branchName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteBranch(Guid branchId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
