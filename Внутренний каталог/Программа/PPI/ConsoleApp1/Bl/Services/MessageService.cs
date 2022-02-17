6af10267b1cc91c3184181baeaf8f14806f76dd4
Ilya <nastekoir@gmail.com>
2022-02-17 20:42:50 +0300

    Смещение предыдущих ЛР во внутренний каталог

--- Внутренний каталог/Программа/PPI/ConsoleApp1/Bl/Services/MessageService.cs
+++ Внутренний каталог/Программа/PPI/ConsoleApp1/Bl/Services/MessageService.cs
@@ -8,12 +8,10 @@ public class MessageService : IMessageService
     {
         //DELETE MessageTable WHERE Id=messageId
     }
-
     public void EditMessage(Guid messageId, string messageText)
     {
         //UPDATE MessageTeble SET MessageText=messageText WHERE Id=messageId
     }
-
     public void SendMessage(Guid branchId, Message message)
     {
         //INSERT MessageTable message.MessageText,message.UserId,message.CreatedDate,branchId
